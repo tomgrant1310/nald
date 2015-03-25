0ed4319b10d856a02c640af8d2adb0d34a002120
Tom Grant <tomgrant1310@gmail.com>
2015-03-25 10:03:46 +0000

    upgrade to megamenu 2.17

--- sites/all/modules/md_megamenu/templates/menu/md_megamenu_dropdown_tab.tpl.php
+++ sites/all/modules/md_megamenu/templates/menu/md_megamenu_dropdown_tab.tpl.php
@@ -1,4 +1,7 @@
 <li class="<?php print $tab_classes?>">
-<?php print $tab->content;?>
-<?php print $tab_content; ?>
+  <?php if(count($items) > 0): ?>
+    <span class="mdtoggle-sub"></span>
+  <?php endif; ?>
+  <?php print $tab->content;?>
+  <?php print $tab_content; ?>
 </li>
--- sites/all/modules/md_megamenu/templates/menu/md_megamenu_theme.tpl.php
+++ sites/all/modules/md_megamenu/templates/menu/md_megamenu_theme.tpl.php
@@ -1,12 +1,24 @@
-<div id="megamenu-<?php print $mid;?>" class="<?php print $menu_classes; ?>">
-  <ul id="mdmegamenu-<?php print $mid;?>" class="megamenu clearfix">
-    <?php foreach ($menu_tabs as $key => $tab):?>
+
+<div id="megamenu-<?php print $mid; ?>" class="<?php print $menu_classes; ?>"
+     data-type-responsive="<?php print $menu_settings['responsive']['res_width']; ?>">
+  <span
+    class="mdtoggle-nav"><?php print $menu_settings['responsive']['res_text']; ?></span>
+  <ul id="mdmegamenu-<?php print $mid; ?>" class="megamenu clearfix">
+    <?php foreach ($menu_tabs as $key => $tab): ?>
       <?php if ($tab->settings['general']['is_dropdown_menu']): ?>
-        <?php print theme('megamenu_dropdown_tab', array('tab' => $tab, 'key' => $key, 'settings' => $menu_settings))?>
+        <?php print theme('megamenu_dropdown_tab', array(
+          'tab' => $tab,
+          'key' => $key,
+          'settings' => $menu_settings
+        )) ?>
       <?php else: ?>
-        <?php print theme('megamenu_tab_theme', array('key' => $key, 'tab' => $tab, 'settings' => $menu_settings))?>
+        <?php print theme('megamenu_tab_theme', array(
+          'key' => $key,
+          'tab' => $tab,
+          'settings' => $menu_settings
+        )) ?>
       <?php endif; ?>
-    <?php endforeach;?>
+    <?php endforeach; ?>
   </ul>
   <?php print $show_button; ?>
 </div>
